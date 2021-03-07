5202626bbbaa877c3158af08971b7f254305f973
Igor Novak <bezengi@gmail.com>
2021-03-07 22:39:28 +0100

    Tune layout styles

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./SearchForm.css";
 
 export default function SearchForm(props: {
   query: string;
@@ -7,18 +8,18 @@ export default function SearchForm(props: {
   const [query, setQuery] = useState(props.query);
   return (
     <form
+      className="search-form"
       onSubmit={(e) => {
         e.preventDefault();
         props.onSubmit(query);
       }}
     >
       <input
+        className="form-input"
         value={query}
-        onChange={(e) => {
-          setQuery(e.target.value);
-        }}
+        onChange={(e) => setQuery(e.target.value)}
       />
-      <input type="submit" />
+      <input className="form-submit" type="submit" value="Search" />
     </form>
   );
 }
