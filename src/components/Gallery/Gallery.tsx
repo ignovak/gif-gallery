54b40fb47c725fea0eaa9f0040ea102f24f80a24
Igor Novak <bezengi@gmail.com>
2021-03-07 20:54:51 +0100

    Fetch gifs

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,3 +1,28 @@
+import { useEffect, useState } from "react";
+import apiKey from "../../giphy_api_key.json";
+
 export default function Gallery() {
-  return <div>gallery</div>;
+  const [state, setState] = useState([]);
+  const query = "cats";
+  useEffect(() => {
+    fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=10`,
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        result.data.map(
+          (item: { images: { preview_gif: { url: string } } }) =>
+            item.images.preview_gif.url,
+        ),
+      )
+      .then((urls) => setState(urls));
+  }, [query]);
+
+  return (
+    <div>
+      {state.map((image, i) => (
+        <img key={i} src={image} alt={image} />
+      ))}
+    </div>
+  );
 }
