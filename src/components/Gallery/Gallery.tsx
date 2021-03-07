10e4fc6a8b83d184b472646c1c889ed12ae321fa
Igor Novak <bezengi@gmail.com>
2021-03-07 21:01:32 +0100

    Add gif titles to alt attribute

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,27 +1,41 @@
 import { useEffect, useState } from "react";
 import apiKey from "../../giphy_api_key.json";
 
+interface Gif {
+  title: string;
+  url: string;
+}
+
+function fetchGifs(query: string) {
+  return fetch(
+    `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=10`,
+  )
+    .then((response) => response.json())
+    .then((result) =>
+      result.data.map(
+        (item: {
+          images: { preview_gif: { url: string } };
+          title: string;
+        }) => ({
+          title: item.title,
+          url: item.images.preview_gif.url,
+        }),
+      ),
+    );
+}
+
 export default function Gallery() {
   const [state, setState] = useState([]);
   const query = "cats";
+
   useEffect(() => {
-    fetch(
-      `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=10`,
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.data.map(
-          (item: { images: { preview_gif: { url: string } } }) =>
-            item.images.preview_gif.url,
-        ),
-      )
-      .then((urls) => setState(urls));
+    fetchGifs(query).then((gifs) => setState(gifs));
   }, [query]);
 
   return (
     <div>
-      {state.map((image, i) => (
-        <img key={i} src={image} alt={image} />
+      {state.map((gif: Gif, i) => (
+        <img key={i} src={gif.url} alt={gif.title} />
       ))}
     </div>
   );
